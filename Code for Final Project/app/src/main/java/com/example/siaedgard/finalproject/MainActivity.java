0309784d8b9aa4f300bc700b7129975380b42c01
Jean-Marie <jndah045@uottawa.ca>
2018-10-24 11:20:34 -0400

    Done without the bonus point

--- Code for Final Project/app/src/main/java/com/example/siaedgard/finalproject/MainActivity.java
+++ Code for Final Project/app/src/main/java/com/example/siaedgard/finalproject/MainActivity.java
@@ -2,27 +2,78 @@ package com.example.siaedgard.finalproject;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
-
 
 public class MainActivity extends AppCompatActivity {
 
     EditText LastName, FirstName ,Birthday , PostalCode, userProfile,Password;
     TextView result;
     Button ButtonFinish;
+    private Spinner spinner;
+    private String [] answers = new String [7];
+    public static final String[] paths = {"Admin", "Home owner", "Service provider"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        spinner = (Spinner)findViewById(R.id.DropDown1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_spinner_item,paths);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        Password = (EditText) findViewById(R.id.Password);
+        userProfile = (EditText) findViewById(R.id.userProfile);
+        PostalCode = (EditText) findViewById(R.id.PostalCode);
+        Birthday = (EditText) findViewById(R.id.Birthday);
+        FirstName = (EditText) findViewById(R.id.FirstName);
+        LastName = (EditText) findViewById(R.id.LastName);
+
+
     }
 
     public void OnFinish(View view) {
-        Intent intent = new Intent(getApplicationContext(), WelcomePage.class);
-        startActivityForResult (intent,0);
+
+        answers[0] = LastName.getText().toString();
+        answers[1]=  FirstName.getText().toString();
+        answers[2] = Birthday.getText().toString();
+        answers[3] = PostalCode.getText().toString();
+        answers[4] = spinner.getSelectedItem().toString();
+        answers[5] = userProfile.getText().toString();
+        answers[6] =Password.getText().toString();
+        boolean invalid = false;
+        TextView text = (TextView)findViewById(R.id.textView);
+        int i =0;
+        while(invalid == false && i<answers.length ) {
+            if (answers[i].isEmpty()) {
+                invalid = true;
+            }
+            i++;
+        }
+        if( invalid == true){
+
+            // check the other conditions in the loop above
+            AlertDialog.Builder  alert = new AlertDialog.Builder(this);
+            alert.setTitle("Empty field alert");
+            alert.setMessage("You need to fill up all the field");
+            alert.setPositiveButton("OK",null);
+            alert.show();
+        } else{
+            Intent intent = new Intent(this, WelcomePage.class);
+            intent.putExtra("USER_TYPE", answers[4]);
+            intent.putExtra("FIRST_NAME",  answers[1]);
+            intent.putExtra("LAST_NAME", answers[0]);
+            startActivity(intent);
+        }
+
     }
+
 }
--- Code for Final Project/app/src/main/java/com/example/siaedgard/finalproject/WelcomePage.java
+++ Code for Final Project/app/src/main/java/com/example/siaedgard/finalproject/WelcomePage.java
@@ -1,14 +1,69 @@
 package com.example.siaedgard.finalproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
 
 public class WelcomePage extends AppCompatActivity {
+
+
+    private static final String[] ActionBarForAdmin = {"List of service provider", "Create service", "Modify rate"};
+    private static final String[] ActionBarForHomeOwner = {"Search for service", "Book a service", "Rate a service"};
+    private static final String[] ActionBarForServiceProvider = {"Modify profil", "Associate with service", "Enter Availabilities"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome_page);
+        TextView FirsttxtView = findViewById(R.id.sessionText);
+        TextView SecondtxtView = findViewById(R.id.welcomeText);
+        Intent intent = getIntent();
+
+        Bundle bd = intent.getExtras();
+        if(bd != null)
+        {
+            String adminSession = bd.get("USER_TYPE") + " Session";
+            if (bd.get("USER_TYPE").equals(MainActivity.paths[0])) {
+                Button ButtonAction1 = findViewById(R.id.Action1);
+                ButtonAction1.setText(ActionBarForAdmin[0]);
+                Button ButtonAction2 = findViewById(R.id.Action2);
+                ButtonAction2.setText(ActionBarForAdmin[1]);
+                Button ButtonAction3 = findViewById(R.id.Action3);
+                ButtonAction3.setText(ActionBarForAdmin[2]);
+                String getName = "Welcome "+(String) bd.get("FIRST_NAME")+ " " + bd.get("LAST_NAME");
+                FirsttxtView.setText(adminSession);
+                SecondtxtView.setText(getName);
+            } else if (bd.get("USER_TYPE").equals(MainActivity.paths[1])) {
+                Button ButtonAction1 = findViewById(R.id.Action1);
+                ButtonAction1.setText(ActionBarForHomeOwner[0]);
+                Button ButtonAction2 = findViewById(R.id.Action2);
+                ButtonAction2.setText(ActionBarForHomeOwner[1]);
+                Button ButtonAction3 = findViewById(R.id.Action3);
+                ButtonAction3.setText(ActionBarForHomeOwner[2]);
+                String getName = "Welcome "+(String) bd.get("FIRST_NAME")+ " " + bd.get("LAST_NAME");
+                FirsttxtView.setText(adminSession);
+                SecondtxtView.setText(getName);
+            } else {
+                Button ButtonAction1 = findViewById(R.id.Action1);
+                ButtonAction1.setText(ActionBarForServiceProvider[0]);
+                Button ButtonAction2 = findViewById(R.id.Action2);
+                ButtonAction2.setText(ActionBarForServiceProvider[1]);
+                Button ButtonAction3 = findViewById(R.id.Action3);
+                ButtonAction3.setText(ActionBarForServiceProvider[2]);
+                String getName = "Welcome "+(String) bd.get("FIRST_NAME")+ " " + bd.get("LAST_NAME");
+                FirsttxtView.setText(adminSession);
+                SecondtxtView.setText(getName);
+            }
+        }
+    }
+
+    public void Action1Click(View view) {
+        ;
     }
 
 
