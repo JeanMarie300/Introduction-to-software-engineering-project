f9fe4e0309faf4265cf700ebf735a6cd0a2e87d1
alaraOzbir <43187304+alaraOzbir@users.noreply.github.com>
2018-11-10 12:17:42 -0500

    Update SecondDeliverableTest.java

--- First Deliverable Code/app/src/test/java/com/example/siaedgard/finalproject/SecondDeliverableTest.java
+++ First Deliverable Code/app/src/test/java/com/example/siaedgard/finalproject/SecondDeliverableTest.java
@@ -17,7 +17,51 @@ public class SecondDeliverableTest {
         Services service = new Services("plumber", "10");
         assertEquals("Check the name of the service", "plumber", service.getName());
     }
+    @Test
+    public void checkFirstName(){
+        User user = new User("Jake","Peralta","1988-05-21","K1C 1T1", "Home Owner", "jPera", "JPera1988" );
+        assertEquals("Check the first name of the user", "Jake", user.getFirstName());
+    }
 
+    @Test
+    public void checkLastName(){
+        User user = new User("Jake","Peralta","1988-05-21","K1C 1T1", "Home Owner", "jPera", "JPera1988" );
+        assertEquals("Check the last name of the user", "Peralta", user.getLastName());
+    }
+
+    @Test
+    public void checkUserType(){
+        User user = new User("Jake","Peralta","1988-05-21","K1C 1T1", "Home Owner", "jPera", "JPera1988" );
+        assertEquals("Check the user type", "Home Owner", user.getUserType());
+    }
+
+    @Test
+    public void checkActionBar(){
+        User user = new User("Jake","Peralta","1988-05-21","K1C 1T1", "Home Owner", "jPera", "JPera1988" );
+
+        if (user.getUserType().equals("Home Owner")){
+            String [] expected = {"Search for service", "Book a service", "Rate a service"};
+            String [] actual = WelcomePage.ActionBarForHomeOwner;
+            assertEquals("Check bar action button [1]", expected[0], actual[0]);
+            assertEquals("Check bar action button [2]", expected[0], actual[0]);
+            assertEquals("Check bar action button [3]", expected[0], actual[0]);
+        }
+        else if (user.getUserType().equals("Admin")){
+            String [] expected = {"Search for service", "Book a service", "Rate a service"};
+            String [] actual = WelcomePage.ActionBarForAdmin;
+            assertEquals("Check bar action button [1]", expected[0], actual[0]);
+            assertEquals("Check bar action button [2]", expected[0], actual[0]);
+            assertEquals("Check bar action button [3]", expected[0], actual[0]);
+        }
+        else{
+            String [] expected = {"Search for service", "Book a service", "Rate a service"};
+            String [] actual = WelcomePage.ActionBarForServiceProvider;
+            assertEquals("Check bar action button [1]", expected[0], actual[0]);
+            assertEquals("Check bar action button [2]", expected[0], actual[0]);
+            assertEquals("Check bar action button [3]", expected[0], actual[0]);
+        }
+
+    }
 
    /* @Test
     public void checkProductId() {
